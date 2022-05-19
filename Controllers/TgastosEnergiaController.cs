df9b076f92610e0b9c2b481355c5df447dbe270c
capgemini-rodrigo.erasmo <capgemini-rodrigo.erasmo@ailos.coop.br>
2022-05-19 18:41:20 -0300

    Add project files.

---
+++ Controllers/TgastosEnergiaController.cs
@@ -0,0 +1,16 @@
+﻿using ConsultasMVC.Controllers.abstractions;
+using ConsultasMVC.dbenersave;
+using EnerSave.Views.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace ConsultasMVC.Controllers
+{
+    public class TgastosEnergiaController : Controller
+    {
+        
+
+       
+    }
+}
---
+++ Models/dbenersave/DbenersaveContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ConsultasMVC.dbenersave
+{
+    public partial class DbenersaveContext : DbContext
+    {
+        public DbenersaveContext()
+        {
+        }
+
+        public DbenersaveContext(DbContextOptions<DbenersaveContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TgastosAgua> TgastosAgua { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TgastosAgua>(entity =>
+            {
+                entity.ToTable("tgastos_agua", "dbenersave");
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("id");
+
+                entity.Property(e => e.MetrosCubicos)
+                    .HasColumnName("metros_cubicos");
+
+                entity.Property(e => e.Periodo)
+                    .HasColumnName("periodo");
+
+                entity.Property(e => e.UsuarioId)
+                    .HasColumnName("usu_id");
+
+            });
+        }
+    }
+}
