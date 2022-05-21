cb653f162b3b12804cef24efe2b9d1e6a543f49c
Ana Quadros <ana.quadros@cakeerp.com>
2022-05-21 15:26:49 -0300

    Criacao das Views de criar conta e login

---
+++ EnerSave/Views/ViewModels/UsuarioViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EnerSave.Views.ViewModels
+{
+    public class UsuarioViewModel
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public int Senha { get; set; }
+        public string Nome { get; set; }
+        public int Cpf { get; set; }
+        public string Endereco { get; set; }
+        public int Telefone { get; set; }
+
+
+
+    }
+}
